3dee134656086ed8c7ee7639f4ddcbc1b31c884d
SamuelJLeonard <SamuelJLeonard@gmail.com>
2017-09-10 15:24:53 -0700

    Create clustering_function.R

---
+++ 2. Clustering Function/clustering_function.R
@@ -0,0 +1,36 @@
+set.clusters <- function(k, distances, Gk, n){
+  regions <- seq(1:n)
+  not.centers <- regions[! regions %in% Gk]
+
+  distances.to.centers <- distances[not.centers, Gk]
+  pairing.vector <- apply(distances.to.centers, 1, which.min)
+
+  center.matrix <- matrix(c(Gk, Gk), ncol = 2)
+  not.center.matrix <- matrix(c(not.centers, Gk[pairing.vector]), ncol = 2)
+  pairing.matrix <- rbind(center.matrix, not.center.matrix)
+
+  C.not.centers <- list()
+  
+  for (i in 1:length(Gk)){
+    C.not.centers[[i]] <- not.centers[which(pairing.vector == i)]
+  } 
+  
+  C <- list()
+  
+  for (i in 1:length(Gk)){
+    C[[i]] <- Gk[i]
+  }
+
+  for (i in 1:length(Gk)){
+    C[[i]] <- append(C[[i]], C.not.centers[[i]])
+  }
+  
+  return(C)
+}
+ 
+
+
+
+
+
+
